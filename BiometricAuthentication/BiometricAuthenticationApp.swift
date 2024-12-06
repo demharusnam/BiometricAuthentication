5ac37719e1cb0aeed7ae7ac8b70a209cdea7ae23
Mansur Ahmed <demharusnam@icloud.com>
2024-12-06 13:25:04 -0800

    Initial Commit

---
+++ BiometricAuthentication/BiometricAuthenticationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiometricAuthenticationApp.swift
+//  BiometricAuthentication
+//
+//  Created by Mansur Ahmed on 12/6/24.
+//
+
+import SwiftUI
+
+@main
+struct BiometricAuthenticationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
